a36c7667944114991f260c803b1bfe2fb16d80cf
Devanshu <devanshukd007@gmail.com>
2023-08-13 13:22:36 +0530

    HW 01 : Ques 05 : Comparison Between Two Floating Point Number

--- Lecture 11 - JavaScript (Absent)/JS - Homework - 01/Question_05.js
+++ Lecture 11 - JavaScript (Absent)/JS - Homework - 01/Question_05.js
@@ -1,4 +1,4 @@
-// Ques.5. Write a JS program that reads two floating-point numbers and tests whether they are the same up 
+// Ques.5. Write a JS program that reads two floating-point numbers and tests whether they are the same up
 //         to three decimal places.
 //     Test Data
 //     Input floating-point number: 25.586
@@ -6,8 +6,8 @@
 //     Expected Output :
 //     They are different
 
-let num1 = 25.586
-let num2 = 25.586
+let num1 = 25.586;
+let num2 = 25.589;
 
 // removing decimals
 let newNum1 = num1 * 1000;
@@ -19,14 +19,14 @@ let str02 = newNum2.toString();
 
 // now checking
 isEqual = true;
-for(let i = 0; i < str01.length; i++){
-	if(str01[i] !== str02[i]){
-		isEqual = false;
-	}
+for (let i = 0; i < str01.length; i++) {
+  if (str01[i] !== str02[i]) {
+    isEqual = false;
+  }
 }
 
-if(isEqual){
-	console.log("Same");
+if (isEqual) {
+  console.log("They are Same");
 } else {
-	console.log("Different");
+  console.log("They are Different");
 }
