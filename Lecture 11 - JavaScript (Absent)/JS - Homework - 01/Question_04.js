b7f20267f50fc94d9d61b0160459771f7620d001
Devanshu <devanshukd007@gmail.com>
2023-08-13 13:19:05 +0530

    HW 01 : Ques 04 : Take 3 Numbers & Find the Greatest Number

--- Lecture 11 - JavaScript (Absent)/JS - Homework - 01/Question_04.js
+++ Lecture 11 - JavaScript (Absent)/JS - Homework - 01/Question_04.js
@@ -6,11 +6,11 @@ let num3 = parseInt(prompt("Enter the third number:"));
 
 let greatestNum = num1;
 
-if (num2 > greatestNumber) {
+if (num2 > greatestNum) {
   greatestNum = num2;
 }
 
-if (num3 > greatestNumber) {
+if (num3 > greatestNum) {
   greatestNum = num3;
 }
 
