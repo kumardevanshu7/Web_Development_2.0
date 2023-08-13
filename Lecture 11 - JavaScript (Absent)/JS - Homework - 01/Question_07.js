8c0bc1d929131ce7b78a41d77e1d8038c6daece7
Devanshu <devanshukd007@gmail.com>
2023-08-13 13:28:56 +0530

    HW 01 : Ques 07 : Finding whether it is a Leap Year or not

--- Lecture 11 - JavaScript (Absent)/JS - Homework - 01/Question_07.js
+++ Lecture 11 - JavaScript (Absent)/JS - Homework - 01/Question_07.js
@@ -1,8 +1,9 @@
+// Ques.7. Write a JS program that takes a year from the user and prints whether it is a leap year or not.
 
 let year = parseInt(prompt("Enter a year:"));
 
 if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-    console.log(`${year} is a leap year.`);
+  console.log(`${year} is a leap year.`);
 } else {
-    console.log(`${year} is not a leap year.`);
+  console.log(`${year} is not a leap year.`);
 }
