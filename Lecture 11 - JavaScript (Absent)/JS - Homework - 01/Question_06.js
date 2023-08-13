7469175545497a6c6a5bef177898757d981d2425
Devanshu <devanshukd007@gmail.com>
2023-08-13 13:25:43 +0530

    HW 01 : Ques 06 : Find Number of Days in a Month

--- Lecture 11 - JavaScript (Absent)/JS - Homework - 01/Question_06.js
+++ Lecture 11 - JavaScript (Absent)/JS - Homework - 01/Question_06.js
@@ -1,19 +1,19 @@
+// Ques.6. Write a JS program to find the number of days in a month.
 
 let month = parseInt(prompt("Enter the month (1-12):"));
 
 if (month < 1 || month > 12) {
-    console.log("Invalid month input. Please enter a value between 1 and 12.");
+  console.log("Invalid month input. Please enter a value between 1 and 12.");
 } else {
-   
-    let daysInMonth;
+  let daysInMonth;
 
-    if (month === 2) {
-        daysInMonth = 28;
-    } else if (month === 4 || month === 6 || month === 9 || month === 11) {
-        daysInMonth = 30;
-    } else {
-        daysInMonth = 31;
-    }
+  if (month === 2) {
+    daysInMonth = 28;
+  } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+    daysInMonth = 30;
+  } else {
+    daysInMonth = 31;
+  }
 
-    console.log(`The number of days in month ${month} is: ${daysInMonth}`);
+  console.log(`The number of days in month ${month} is: ${daysInMonth}`);
 }
