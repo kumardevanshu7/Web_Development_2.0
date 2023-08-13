97ce52a044d8bdd02e3209a594df9a06ce2b3c4a
Devanshu <devanshukd007@gmail.com>
2023-08-13 14:13:29 +0530

    Adding More Codes

--- Lecture 12 - JavaScript/6. JS - Homework - 02/2. Three Pattern Questions/app.js
+++ Lecture 12 - JavaScript/6. JS - Homework - 02/2. Three Pattern Questions/app.js
@@ -1,41 +1,48 @@
 // 1st Pattern : Like
 //  * * * * *
 //  * * * *
-//  * * *   
+//  * * *
 //  * *
 //  *
 
 let row = 5;
-for(let i = row; i >= 1; i--){
-
-    let stars = "";
-    for(let j = 1; j <= i; j++){
-       stars+=" *";
-    }
-    console.log(stars);
+for (let i = row; i >= 1; i--) {
+  let stars = "";
+  for (let j = 1; j <= i; j++) {
+    stars += " *";
+  }
+  console.log(stars);
 }
 
+// Showing Errors
 
-// 1st Pattern : Like        
+// 2nd Pattern : Like
 //          *
-//        * * *  
-//      * * * * *    
-//    * * * * * * *       
-//  * * * * * * * * *         
+//        * * *
+//      * * * * *
+//    * * * * * * *
+//  * * * * * * * * *
 
 let newrow = 10;
-for (let i = 1; i <= newrow; i+=2) {
-    let spaces = " ".repeat(row - i + 1);
-    let stars = "* ".repeat(i);
-    console.log(spaces + stars);
+for (let i = 1; i <= newrow; i += 2) {
+  let spaces = " ".repeat(row - i + 1);
+  let stars = "* ".repeat(i);
+  console.log(spaces + stars);
 }
 
 // 3rd - Number Pattern
-//  1 
+//  1
 //  1 2
 //  1 2 3
 //  1 2 3 4
 //  1 2 3 4 5
 
-let new_Row = 5;
-for(let i = 1; i <= row; i++)
+let newRow = 5;
+for (let i = 1; i <= newRow; i++) {
+  let numbers = "";
+  for (let j = 1; j <= i; j++) {
+    numbers += j;
+  }
+
+  console.log(numbers);
+}
