9f651850e437c02c5f11400db9d01c22e0b0e217
Devanshu <devanshukd007@gmail.com>
2023-08-13 13:12:11 +0530

    HW 01 : Ques 02 : Solve Quadratic Equations

--- Lecture 11 - JavaScript (Absent)/JS - Homework - 01/Question_03.js
+++ Lecture 11 - JavaScript (Absent)/JS - Homework - 01/Question_03.js
@@ -1,8 +1,8 @@
-// Given P,R and T calculate Simple Interest.
+// Ques.3. Given P,R and T calculate Simple Interest.
+
 const principal = parseInt(prompt("Enter the principal:"));
 const rate = parseInt(prompt("Enter the rate of interest:"));
 const time = parseInt(prompt("Enter the time: "));
-
 
 const simpleInterest = (principal * rate * time) / 100;
 
