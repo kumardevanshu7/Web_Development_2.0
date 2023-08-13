a3443023510927a05e18a901d8883c19e5f76961
Devanshu <devanshukd007@gmail.com>
2023-08-13 13:53:54 +0530

    HW 02 : Ques 07 : Print all the factors of a given number

--- Lecture 12 - JavaScript/6. JS - Homework - 02/1. Written 11 Questions/Question_07.js
+++ Lecture 12 - JavaScript/6. JS - Homework - 02/1. Written 11 Questions/Question_07.js
@@ -1,8 +1,8 @@
 // Ques.7. Print all the factors of a given number.
 
 let num = 20;
-for(let i = 1; i <= num; i++){
-    if(num % i === 0){
-        console.log(i);
-    }
+for (let i = 1; i <= num; i++) {
+  if (num % i === 0) {
+    console.log(i);
+  }
 }
