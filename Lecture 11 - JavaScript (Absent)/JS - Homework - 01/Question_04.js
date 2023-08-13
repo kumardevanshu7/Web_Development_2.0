139443d8472c39f9e5a91951f20cfc77337173b6
Devanshu <devanshukd007@gmail.com>
2023-08-13 13:15:40 +0530

    HW 01 : Ques 04 : Take 3 Numbers & Find the Greatest Number

--- Lecture 11 - JavaScript (Absent)/JS - Homework - 01/Question_04.js
+++ Lecture 11 - JavaScript (Absent)/JS - Homework - 01/Question_04.js
@@ -1,3 +1,5 @@
+// Ques.4. Write a JS program that takes three numbers from the user and prints the greatest number.
+
 let num1 = parseInt(prompt("Enter the first number:"));
 let num2 = parseInt(prompt("Enter the second number:"));
 let num3 = parseInt(prompt("Enter the third number:"));
