5419f51459921e09dcf414d39fc091f48fd6e528
Devanshu <devanshukd007@gmail.com>
2023-08-13 13:52:05 +0530

    HW 02 : Ques 06 : Check whether the given number is armstrong or not

--- Lecture 12 - JavaScript/6. JS - Homework - 02/1. Written 11 Questions/Question_06.js
+++ Lecture 12 - JavaScript/6. JS - Homework - 02/1. Written 11 Questions/Question_06.js
@@ -1,9 +1,6 @@
 // Ques.6. You are given a number n . You have to check whether the given number is armstrong number
-
 //     e.g : 371
-        
 //         27+343+1 =>371 - is a armstrong number
-
 
 let num = 371;
 let temp = num;
@@ -11,20 +8,20 @@ let temp = num;
 // counting
 let count = 0;
 while (temp !== 0) {
-    temp = Math.floor(temp / 10);
-    count++;
+  temp = Math.floor(temp / 10);
+  count++;
 }
 
 let sum = 0;
 temp = num;
-while(temp !==0){
-    let digit = temp % 10;
-    sum = sum + Math.pow(digit, count);
-    temp = Math.floor(temp / 10);
+while (temp !== 0) {
+  let digit = temp % 10;
+  sum = sum + Math.pow(digit, count);
+  temp = Math.floor(temp / 10);
 }
 
 if (sum === num) {
-    console.log("ARMS");
+  console.log("is a armstrong number");
 } else {
-    console.log("NOT ARMS");
+  console.log("is not a armstrong number");
 }
