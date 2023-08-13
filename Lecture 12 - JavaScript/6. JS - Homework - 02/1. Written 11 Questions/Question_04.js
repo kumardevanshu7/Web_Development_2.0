b162a00f8394b9ab7f672aaffda6667461428053
Devanshu <devanshukd007@gmail.com>
2023-08-13 13:47:04 +0530

    HW 02 : Ques 04 : Print the table for given number

--- Lecture 12 - JavaScript/6. JS - Homework - 02/1. Written 11 Questions/Question_04.js
+++ Lecture 12 - JavaScript/6. JS - Homework - 02/1. Written 11 Questions/Question_04.js
@@ -1,6 +1,5 @@
 // Ques.4. You are given a number n. You have to print the table for that number.
 
-
 // let n = 4;
 
 // for(let i = n; i<=n*10; i+=4){
@@ -10,6 +9,6 @@
 // ------- or -------
 
 let n = 4;
-for(let i = 1; i <= 10; i++){
-    console.log(`${n} x ${i} = ${n*i}`)
+for (let i = 1; i <= 10; i++) {
+  console.log(`${n} x ${i} = ${n * i}`);
 }
