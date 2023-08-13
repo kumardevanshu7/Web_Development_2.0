86ce66662d0bb8389cbf677be1c15f52157f9e18
Devanshu <devanshukd007@gmail.com>
2023-08-13 13:40:36 +0530

    HW 02 : Ques 01 : Print All Even Number upto n.

--- Lecture 12 - JavaScript/6. JS - Homework - 02/1. Written 11 Questions/Question_01.js
+++ Lecture 12 - JavaScript/6. JS - Homework - 02/1. Written 11 Questions/Question_01.js
@@ -2,8 +2,8 @@
 
 let n = 20;
 
-for(let i = 1; i <= n; i++){
-    if(i % 2 === 0){
-        console.log(i);
-    }
+for (let i = 1; i <= n; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
 }
